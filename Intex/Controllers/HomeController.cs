c0e1c1ceac9a3e49b5df4a93d42686a428d845ba
Kyle <kylebharrison16@gmail.com>
2018-12-05 20:58:03 -0700

    added manager view

--- Intex/Controllers/HomeController.cs
+++ Intex/Controllers/HomeController.cs
@@ -13,8 +13,6 @@ namespace Intex.Controllers
             return View();
         }
 
-
-        //Testy test testt test
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
